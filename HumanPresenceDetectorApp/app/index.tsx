12f202b5ba22ab4fd03e0c52d3d58ae9eaacb7e4
Niko4u <Niklas@fam-email.de>
2025-06-28 13:32:03 +0200

    remove headers

--- HumanPresenceDetectorApp/app/index.tsx
+++ HumanPresenceDetectorApp/app/index.tsx
@@ -2,6 +2,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AddDeviceScreen from './screens/AddDeviceScreen';
 import MyDevicesScreen from './screens/MyDevicesScreen';
+import {SafeAreaView} from "react-native";
 
 
 const Tab = createBottomTabNavigator();
@@ -9,8 +10,8 @@ const Tab = createBottomTabNavigator();
 export default function Index() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="My Devices" component={MyDevicesScreen} />
-      <Tab.Screen name="Add Device" component={AddDeviceScreen} />
+      <Tab.Screen name="My Devices" component={MyDevicesScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="Add Device" component={AddDeviceScreen} options={{ headerShown: false }}/>
     </Tab.Navigator>
   );
 }
