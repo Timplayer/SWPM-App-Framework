f6e97e01660265813fc645351c5ffe2e43dda3c1
Tim Jaeschke <tim.jae@gmx.de>
2025-06-29 14:32:01 +0200

    feat: enhance AddDeviceCodeScreen to handle device connection with local search parameters

--- HumanPresenceDetectorApp/app/screens/AddDeviceCodeScreen.tsx
+++ HumanPresenceDetectorApp/app/screens/AddDeviceCodeScreen.tsx
@@ -1,26 +1,35 @@
-import { useRouter } from 'expo-router';
+import { useLocalSearchParams } from 'expo-router';
 import React from 'react';
 import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { GlobalStyles } from '../styles/GlobalStyles';
 import {ESPDevice} from "@orbital-systems/react-native-esp-idf-provisioning";
 
-export default function AddDeviceCodeScreen(props: {setState: (state: "searchWifi") => void, device: ESPDevice}) {
-  const {setState, device} = props;
+export default function AddDeviceCodeScreen(props: {setState: (state: "searchWifi") => void, device?: ESPDevice}) {
+  const {setState} = props;
+  const params = useLocalSearchParams();
+  const device: ESPDevice | undefined = params.device ? JSON.parse(params.device as string) : props.device;
+  const pop: string | undefined = params.pop as string;
+
   const [code, onChangeCode] = React.useState('');
   const [loading, setLoading] = React.useState(false);
 
   const onCheckCode = React.useCallback(async () => {
+      if (!device) {
+          console.log("Device is undefined.");
+          return;
+      }
+      console.log("Attempting to connect device:", device);
+      console.log("POP:", pop);
+      console.log("Code (if POP is not present):", code);
       try {
           setLoading(true);
-          await device.connect(code);
+          await device.connect(pop || code);
           setState("searchWifi");
       } catch (error) {
-          console.error(error);
+          console.error("Device connection error:", error);
           setLoading(false);
-          // TODO remove
-          setState("searchWifi");
       }
-  }, [code])
+  }, [code, device, pop])
 
 
   return (
